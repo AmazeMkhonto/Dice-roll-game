557a1fd3efeacfe2d20a5ffd7fa39f2c26340dfa
amazemkhonto <amaze.mkhonto2@gmail.com>
2023-07-12 20:14:58 +0200

    late

--- diceroll/Program.cs
+++ diceroll/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Game{
+    public static void Main(string[] args)
+    {
+       int number = numberGenerator();
+       System.Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries."); 
+       string userInput = Console.ReadLine();
+       int guess = int.Parse(userInput);
+        int count = 1;
+        while (guess != number){
+             
+
+            if(count > 3){
+                System.Console.WriteLine("ran out of guesses");
+                break;
+            }
+            System.Console.WriteLine("guess is wrong, guess again");
+            System.Console.WriteLine("this is what count is: "+count);
+            userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+            count++;
+       }
+       if(guess == number){
+        System.Console.WriteLine("you won");
+       } else {
+        System.Console.WriteLine("you lost bitch");
+       }
+    }
+
+    public static int numberGenerator(){
+        Random random = new Random();
+        int number = random.Next(1,7);
+        return number;
+    }
+
+
+}
